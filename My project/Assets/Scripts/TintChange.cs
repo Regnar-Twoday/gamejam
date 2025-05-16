8af1302fbfd66e7c4c6b1cb882c5f5af820d7a74
Regnar-Twoday <regnar.vedsted@twoday.com>
2025-05-16 20:30:02 +0200

    lol

--- My project/Assets/Scripts/TintChange.cs
+++ My project/Assets/Scripts/TintChange.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using NUnit.Framework;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class TintChange : MonoBehaviour
@@ -10,19 +11,41 @@ public class TintChange : MonoBehaviour
     public Color yellow = Color.yellow;
     public Color green = Color.green;
 
+    public float interval;
+    public float alpha;
+    private int current = 0;
+
     public List<Color> colors = new();
+
 
     private SpriteRenderer spriteRenderer;
     private float timeElapsed;
 
     void Start()
     {
+        colors.Add(red);
+        colors.Add(blue);
+        colors.Add(yellow);
+        colors.Add(green);
         spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        timeElapsed += Time.deltaTime;  // Increase time
+
+        // If time elapsed is greater than the interval, toggle the color
+        if (timeElapsed >= interval)
+        {
+            spriteRenderer.color = new Color(colors[current].r, colors[current].g, colors[current].b, alpha);
+
+            current += 1;
+
+            current = current % colors.Count;
+
+            // Reset timer
+            timeElapsed = 0f;
+        }
     }
 }
